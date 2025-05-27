cf8394c9022e9df45d9db540efd51151fc40577f
juanjore0 <juan.arango7@utp.edu.co>
2025-05-26 23:10:40 -0500

    update

--- controller/doctor_gui_controller.py
+++ controller/doctor_gui_controller.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QMessageBox, QDialog, QTableWidgetItem
 from GUI.create_doctor_dialog import CreateDoctorDialog
 from . import controller_doctor
-from controller.storage import hospitals
+from controller.storage import doctors  # Lista global de doctores no asignados
 
 class DoctorGUIController:
     def __init__(self, view):
@@ -28,27 +28,25 @@ class DoctorGUIController:
         if row < 0:
             return
         dni_item = self.view.doctor_table.item(row, 2)
-        hospital_item = self.view.doctor_table.item(row, 0)
-        if not dni_item or not hospital_item:
+        if not dni_item:
             QMessageBox.warning(self.view, "Error", "Fila inválida seleccionada.")
             return
         dni = dni_item.text()
-        hospital_name = hospital_item.text()
-        success = controller_doctor.delete_doctor(hospital_name, dni)
+        success = controller_doctor.delete_doctor(dni)
         if success:
             self.refresh_table()
         else:
             QMessageBox.warning(self.view, "Error", "No se pudo eliminar el doctor.")
 
     def refresh_table(self):
+        # Actualizar la lista de doctores antes de mostrarla
+        global doctors
+        doctors = controller_doctor.get_all_doctors()  # Asegúrate de que este método exista y retorne la lista actualizada
         self.view.doctor_table.setRowCount(0)
-        row_index = 0
-        for hospital in hospitals:
-            for doctor in hospital.doctors:
-                self.view.doctor_table.insertRow(row_index)
-                self.view.doctor_table.setItem(row_index, 0, QTableWidgetItem(getattr(hospital, 'hospital_name', str(hospital))))
-                self.view.doctor_table.setItem(row_index, 1, QTableWidgetItem(getattr(doctor, 'doctor_name', str(doctor))))
-                self.view.doctor_table.setItem(row_index, 2, QTableWidgetItem(getattr(doctor, 'dni', '')))
-                self.view.doctor_table.setItem(row_index, 3, QTableWidgetItem(getattr(doctor, 'speciality', '')))
-                row_index += 1
+        for i, doctor in enumerate(doctors):
+            self.view.doctor_table.insertRow(i)
+            self.view.doctor_table.setItem(i, 0, QTableWidgetItem("No asignado"))  # Hospital vacío
+            self.view.doctor_table.setItem(i, 1, QTableWidgetItem(doctor.doctor_name))
+            self.view.doctor_table.setItem(i, 2, QTableWidgetItem(doctor.dni))
+            self.view.doctor_table.setItem(i, 3, QTableWidgetItem(doctor.speciality))
         self.view.doctor_table.resizeColumnsToContents()
--- controller/storage.py
+++ controller/storage.py
@@ -1,1 +1,2 @@
 hospitals = []
+doctors = []
